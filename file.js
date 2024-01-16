8fb9bf86646605c022dcdddf51c6a5115bda8b5f
kaku <144920398+kaku-coder@users.noreply.github.com>
2024-01-16 11:39:42 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,10 @@
+var menu=document.querySelector(".manu");
+menu.addEventListener("click",()=>{
+    menu.classList.toggle("openmenu")
+}) 
+
+
+var menu2=document.querySelector(".manu2");
+menu2.addEventListener("click",()=>{
+    menu2.classList.toggle("openmenu2")
+}) 
